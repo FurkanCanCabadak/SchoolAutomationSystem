546622a075812366c153bb310cf61d7954a5d2af
Furkan Can CABADAK <furkancancabadak@gmail.com>
2023-01-29 19:01:48 +0300

    AdminDepartment finished.

--- SchoolAutomationSystem/Controllers/AdminDepartmentController.cs
+++ SchoolAutomationSystem/Controllers/AdminDepartmentController.cs
@@ -31,6 +31,33 @@ namespace SchoolAutomationSystem.Controllers
                                   "Department Add Successful" : "Department Add Failed";
             return RedirectToAction("Index");
         }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var department = departmentRepository.Detail(id);
+            if (department != null)
+            {
+                DepartmentFaculty model = new DepartmentFaculty();
+                model.DepartmentList= departmentRepository.List();
+                model.FacultyList= facultyRepository.List();
+                model.SingleDepartment = department;
+                model.SingleFaculty = facultyRepository.Detail(department.FacultyId);
+            
+                return View(model);
+            }
+
+            return RedirectToAction("Index");
+
+        }
+        [HttpPost]
+        public ActionResult Edit(Department department)
+        {
+            ViewBag.Message = departmentRepository.Edit(department) ?
+                                   "Department Edit Successful" :
+                                   "Department Edit Failed";
+
+            return Redirect(Request.UrlReferrer.ToString());
+        }
         public ActionResult Delete(int id)
         {
             TempData["Message"] = departmentRepository.Delete(id) ?
